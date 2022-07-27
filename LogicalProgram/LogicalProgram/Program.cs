9c40b49713dd7f4114b1e8fa73e5b36ccbe4cbc8
madhums0411 <madhums41175@gmail.com>
2022-07-27 13:01:54 +0530

    Check the Perfect Number

--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -1,6 +1,24 @@
 ﻿using LogicalProgram;
 
 Console.WriteLine("Welcome to Logical Programs");
-Console.WriteLine("Welcome to Fibonacci Series problem");
-FibonacciSeriesProblem fibonacciSeriesProblem = new FibonacciSeriesProblem();
-fibonacciSeriesProblem.FibonacciSeries();
+Console.WriteLine("Enter 1 for Fibonacci Series");
+Console.WriteLine("Enter 2 for Perfect Numbers");
+
+
+int Problem = Convert.ToInt32(Console.ReadLine());
+
+
+switch(Problem)
+{
+
+    case 1:
+        FibonacciSeriesProblem fibonacciSeriesProblem = new FibonacciSeriesProblem();
+        fibonacciSeriesProblem.FibonacciSeries();
+        break;
+
+    case 2:
+        PerfectNumberProblem perfectNumberProblem = new PerfectNumberProblem();
+        perfectNumberProblem.PerfectNumber();
+        break;
+
+}
