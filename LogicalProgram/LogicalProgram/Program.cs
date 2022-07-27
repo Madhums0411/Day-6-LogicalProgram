067c1750ac203e3cd075b4b8f8c35905b38a4174
madhums0411 <madhums41175@gmail.com>
2022-07-27 20:18:39 +0530

    Generate Random coupon number

--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Enter 1 for Fibonacci Series");
 Console.WriteLine("Enter 2 for Perfect Numbers");
 Console.WriteLine("Enter 3 for Prime numbers");
 Console.WriteLine("Enter 4 for Reverse number");
+Console.WriteLine("Enter 5 for Coupon Number");
 
 
 int Problem = Convert.ToInt32(Console.ReadLine());
@@ -32,5 +33,9 @@ switch(Problem)
         ReverseNumberProblem reverseNumberProblem = new ReverseNumberProblem();
         reverseNumberProblem.ReverseNumber();
         break;
-
+    case 5:
+        Console.Write("How many numbers you wants to generate: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        CouponNumbersProblem.CouponNumber(N);
+        break;
 }
