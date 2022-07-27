6dd15feb6aee61de9f5c1a49a9731008e7d9ff21
madhums0411 <madhums41175@gmail.com>
2022-07-27 14:10:01 +0530

    Reverse number problem

--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Welcome to Logical Programs");
 Console.WriteLine("Enter 1 for Fibonacci Series");
 Console.WriteLine("Enter 2 for Perfect Numbers");
 Console.WriteLine("Enter 3 for Prime numbers");
+Console.WriteLine("Enter 4 for Reverse number");
 
 
 int Problem = Convert.ToInt32(Console.ReadLine());
@@ -25,6 +26,11 @@ switch(Problem)
     case 3:
         PrimeNumberProblem primeNumberProblem = new PrimeNumberProblem();
         primeNumberProblem.PrimeNumber();
+        break;
+
+    case 4:
+        ReverseNumberProblem reverseNumberProblem = new ReverseNumberProblem();
+        reverseNumberProblem.ReverseNumber();
         break;
 
 }
