b96f13d287fcda6f10ef370359a0db6aeebcd9af
madhums0411 <madhums41175@gmail.com>
2022-07-27 12:30:59 +0530

    Fibonacci Series problem

--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -1,2 +1,6 @@
-﻿
+﻿using LogicalProgram;
+
 Console.WriteLine("Welcome to Logical Programs");
+Console.WriteLine("Welcome to Fibonacci Series problem");
+FibonacciSeriesProblem fibonacciSeriesProblem = new FibonacciSeriesProblem();
+fibonacciSeriesProblem.FibonacciSeries();
